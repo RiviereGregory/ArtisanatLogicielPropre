8a3037103c9b045b050c27ab5e9b4a3437451803
RiviereGregory <gregory.riviere@laposte.net>
2024-04-05 18:06:00 +0200

    refactor: permier test

--- VideoStore/src/test/java/gri/riverjach/videostore/CustomerTest.java
+++ VideoStore/src/test/java/gri/riverjach/videostore/CustomerTest.java
@@ -1,18 +1,28 @@
 package gri.riverjach.videostore;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CustomerTest {
 
-    @Test
-    void regularMovie_OneDay() throws Exception{
-        Customer customer = new Customer();
-        customer.addRental("RegularMovie",1);
-        assertEquals(1.5, customer.getRentalFee(), 0.001);
-        assertEquals(1,customer.getRenterPoints());
+    private Customer customer;
 
+    @BeforeEach
+    void setUp() throws Exception {
+        customer = new Customer();
+    }
+
+    private void assertFeeAndPoints(double fee, int point) {
+        assertEquals(fee, customer.getRentalFee(), 0.001);
+        assertEquals(point, customer.getRenterPoints());
+    }
+
+    @Test
+    void regularMovie_OneDay() throws Exception {
+        customer.addRental("RegularMovie", 1);
+        assertFeeAndPoints(1.5, 1);
     }
 
 }
