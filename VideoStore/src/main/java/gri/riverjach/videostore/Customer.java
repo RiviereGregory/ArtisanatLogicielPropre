3e5fbffcfb3cce06810c02f967f55bb94f215e1b
RiviereGregory <gregory.riviere@laposte.net>
2024-04-05 18:32:04 +0200

    feat: exigence 4 sur les films enfants Vert

--- VideoStore/src/main/java/gri/riverjach/videostore/Customer.java
+++ VideoStore/src/main/java/gri/riverjach/videostore/Customer.java
@@ -3,13 +3,18 @@ package gri.riverjach.videostore;
 public class Customer {
 
     private int days;
+    private String title;
 
     public void addRental(String title, int days) {
         this.days = days;
+        this.title = title;
     }
 
-    public double getRentalFee() {
-        return applyGracePeriod(150, 3);
+    public int getRentalFee() {
+        if ("RegularMovie".equalsIgnoreCase(title)) {
+            return applyGracePeriod(150, 3);
+        }
+        return 100;
     }
 
     public int getRenterPoints() {
