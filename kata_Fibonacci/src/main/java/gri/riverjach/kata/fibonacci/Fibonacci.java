fa05f1f821a3133a275b826720e414e31bdf3666
RiviereGregory <gregory.riviere@laposte.net>
2024-04-19 18:03:30 +0200

    feat: initial commit pour kata Fibonacci avec test rouge pour nombre = 0

---
+++ kata_Fibonacci/src/main/java/gri/riverjach/kata/fibonacci/Fibonacci.java
@@ -0,0 +1,14 @@
+package gri.riverjach.kata.fibonacci;
+
+import java.math.BigInteger;
+
+public class Fibonacci {
+
+    private Fibonacci() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static BigInteger fibonacciSuite(int number) {
+        return null;
+    }
+}
---
+++ kata_Fibonacci/src/test/java/gri/riverjach/kata/fibonacci/FibonacciTest.java
@@ -0,0 +1,14 @@
+package gri.riverjach.kata.fibonacci;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigInteger;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FibonacciTest {
+    @Test
+    void when0ThenResult1() {
+        assertEquals(BigInteger.ONE, Fibonacci.fibonacciSuite(0));
+    }
+}
