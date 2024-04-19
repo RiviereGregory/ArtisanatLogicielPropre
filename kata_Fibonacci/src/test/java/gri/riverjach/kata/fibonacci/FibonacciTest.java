bb9ebcb7b44c62453710a65cf28cb04067eaa520
RiviereGregory <gregory.riviere@laposte.net>
2024-04-19 18:39:30 +0200

    feat: Ajout tests de validation

--- kata_Fibonacci/src/test/java/gri/riverjach/kata/fibonacci/FibonacciTest.java
+++ kata_Fibonacci/src/test/java/gri/riverjach/kata/fibonacci/FibonacciTest.java
@@ -27,4 +27,23 @@ class FibonacciTest {
         assertEquals(BigInteger.valueOf(3), Fibonacci.fibonacciSuite(3));
     }
 
+    @Test
+    void when4ThenResult5() {
+        assertEquals(BigInteger.valueOf(5), Fibonacci.fibonacciSuite(4));
+    }
+
+    @Test
+    void when5ThenResult8() {
+        assertEquals(BigInteger.valueOf(8), Fibonacci.fibonacciSuite(5));
+    }
+
+    @Test
+    void when6ThenResult13() {
+        assertEquals(BigInteger.valueOf(13), Fibonacci.fibonacciSuite(6));
+    }
+
+    @Test
+    void when40ThenResult165580141() {
+        assertEquals(BigInteger.valueOf(165580141), Fibonacci.fibonacciSuite(40));
+    }
 }
